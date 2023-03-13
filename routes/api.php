e2baf7399ddbdd1cd3b196bef33a3d7c4b3429f0
Jonthan Marois <jnt.marois@gmail.com>
2023-03-13 10:15:13 +0100

    _

--- routes/api.php
+++ routes/api.php
@@ -15,6 +15,10 @@ use App\Http\Controllers\UserController;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
 
 Route::post('/auth/register', [AuthController::class, 'createUser']);
 Route::post('/auth/login', [AuthController::class, 'loginUser']);
